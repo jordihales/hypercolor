a40e74dd046c487aa1f2dd143929555f67252201
jordihales <46314874+jordihales@users.noreply.github.com>
2020-10-05 22:38:46 +0100

    add more gradients

--- src/assets/data/gradients.js
+++ src/assets/data/gradients.js
@@ -1,6 +1,6 @@
 export const gradients = [
   {
-    title: 'Sunset',
+    title: 'Hyper',
     theme: 'Warm',
     colors: 'bg-gradient-to-r from-red-500 via-pink-500 to-yellow-500',
   },
@@ -15,13 +15,53 @@ export const gradients = [
     colors: 'bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400',
   },
   {
-    title: 'Dark Knight',
+    title: 'Gotham',
     theme: 'Monochrome',
-    colors: 'bg-gradient-to-r from-gray-800 via-gray-900 to-black',
+    colors: 'bg-gradient-to-r from-gray-700 via-gray-900 to-black',
   },
   {
-    title: 'Sand',
+    title: 'Sunset',
+    theme: 'Pastel',
+    colors: 'bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100',
+  },
+  {
+    title: 'Mojave',
     theme: 'Warm',
-    colors: 'bg-gradient-to-r from-yellow-400 via-yellow-500 to-orange-500',
+    colors: 'bg-gradient-to-r from-orange-100 via-orange-300 to-orange-500',
+  },
+  {
+    title: 'Beachside',
+    theme: 'Cool',
+    colors: 'bg-gradient-to-r from-orange-200 via-teal-200 to-teal-500',
+  },
+  {
+    title: 'Gunmetal',
+    theme: 'Monochrome',
+    colors: 'bg-gradient-to-r from-gray-200 via-gray-400 to-gray-600',
+  },
+  {
+    title: 'Peachy',
+    theme: 'Pastel',
+    colors: 'bg-gradient-to-r from-red-200 via-red-300 to-yellow-200',
+  },
+  {
+    title: 'Seafoam',
+    theme: 'Cool',
+    colors: 'bg-gradient-to-r from-teal-200 via-teal-300 to-blue-500',
+  },
+  {
+    title: 'Pumpkin',
+    theme: 'Warm',
+    colors: 'bg-gradient-to-r from-orange-200 via-orange-400 to-orange-700',
+  },
+  {
+    title: 'Pandora',
+    theme: 'Cool',
+    colors: 'bg-gradient-to-r from-green-200 via-teal-400 to-purple-700',
+  },
+  {
+    title: 'Valentine',
+    theme: 'Warm',
+    colors: 'bg-gradient-to-r from-red-200 to-red-600',
   },
 ]
--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -7,8 +7,7 @@
     </h1>
     <div class="max-w-xl mx-auto mb-12 font-light tracking-tight text-gray-900">
       <p>
-        A curated collection of beautiful premade gradients using default colors
-        from the
+        A curated collection of beautiful premade gradients using default colors from the
         <a class="text-teal-500" href="https://tailwindcss.com/">Tailwind</a>
         palette as well as a selection of custom color gradients.
       </p>
@@ -25,12 +24,7 @@
         viewBox="0 0 24 24"
         stroke="currentColor"
       >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M19 9l-7 7-7-7"
-        />
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
       </svg>
     </a>
   </div>
