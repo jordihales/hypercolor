8bd3053ced815c63943587a629c3785d486d91ae
markmead <itsmarkmead@gmail.com>
2020-10-06 09:11:29 +0100

    purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: ['./src/*.vue', './src/components/*.vue', './src/assets/data/*.js'],
   theme: {
     extend: {
       fontFamily: {
