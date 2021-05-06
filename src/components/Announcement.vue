dbcd576e267c696b2919f7e5318dc191de910e6b
Mark Mead <50486078+markmead@users.noreply.github.com>
2021-05-06 23:31:40 +0100

    Update Announcement.vue

--- src/components/Announcement.vue
+++ src/components/Announcement.vue
@@ -2,7 +2,7 @@
   <section class="flex items-center justify-center p-3 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
     <p class="text-sm font-medium text-center text-white">
       New product launch! Tailwind Component Library
-      <a href="https://gum.co/uUMQe" class="underline">Get early access now at 50% off &rarr;</a>
+      <a href="https://gum.co/uUMQe" class="underline">Get early access now for $10 &rarr;</a>
     </p>
   </section>
 </template>
