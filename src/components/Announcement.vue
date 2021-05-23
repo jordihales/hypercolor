9075121381be52811bf98b6f61f1bc79a9a997e9
markmead <itsmarkmead@gmail.com>
2021-05-23 12:29:11 +0100

    update banner

--- src/components/Announcement.vue
+++ src/components/Announcement.vue
@@ -2,9 +2,9 @@
   <section class="text-center bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
     <div class="container py-3">
       <p class="text-sm font-medium text-white">
-        80+ UI components built with Tailwind for $10
+        HyperUI: Components for Tailwind CSS! 80+ Components for $10
         <a href="https://gumroad.com/l/uUMQe/hypercolor" class="underline" target="_blank">
-          Buy now at 25% off for Hypercolor users
+          25% OFF for HYPERCOLOR Users
         </a>
       </p>
     </div>
