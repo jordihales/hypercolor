087264f167fdc779075b7a23b58761760f149264
markmead <itsmarkmead@gmail.com>
2021-05-26 20:29:51 +0100

    remove popup

--- src/components/UpgradeButton.vue
+++ src/components/UpgradeButton.vue
@@ -2,7 +2,8 @@
   <li>
     <a
       class="inline-flex items-center p-3 space-x-3 text-gray-500 border border-gray-200 dark:border-gray-700 rounded-xl dark:text-gray-300"
-      href="https://gumroad.com/l/TjkKF?wanted=true"
+      href="https://gumroad.com/a/1027282035"
+      target="_blank"
     >
       <span
         class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 bg-gray-100 rounded-lg dark:bg-gray-800"
