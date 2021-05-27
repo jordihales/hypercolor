d6f12ec06a1333a7186729e25289e3b9728d3b0c
Mark Mead <50486078+markmead@users.noreply.github.com>
2021-05-28 00:56:37 +0100

    Update Announcement.vue

--- src/components/Announcement.vue
+++ src/components/Announcement.vue
@@ -1,8 +1,8 @@
 <template>
-  <section class="text-center bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500" v-if="!hideAnnouncement">
+  <section class="text-center bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
     <div class="container py-3">
       <p class="text-sm font-medium text-white">
-        HyperUI: Components for Tailwind CSS! 80+ Components for $10
+        HyperUI: Components for Tailwind CSS! 90+ Components for $20
         <a href="https://gumroad.com/l/uUMQe/hypercolor" class="underline" target="_blank">
           25% OFF for HYPERCOLOR Users
         </a>
@@ -10,18 +10,3 @@
     </div>
   </section>
 </template>
-
-<script>
-import { validLicense } from '@/utils/auth'
-
-export default {
-  data() {
-    return {
-      hideAnnouncement: false,
-    }
-  },
-  mounted() {
-    this.hideAnnouncement = this.$route.name === 'Validate' || this.$route.name === 'NotFound' || validLicense()
-  },
-}
-</script>
