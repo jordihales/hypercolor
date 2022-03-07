a5e8cfba7e062dceba4c22415c87e3c0883f6be9
Jordi Hales <jordi.hales@thisisfever.co.uk>
2022-03-07 18:01:17 +0000

    fix/move banner gradient styles to headings for Safari fix

--- components/shared/Banner.vue
+++ components/shared/Banner.vue
@@ -1,10 +1,16 @@
 <template>
   <section class="relative text-center">
     <div class="container py-16 sm:py-24 lg:py-32">
-      <div class="flex flex-col text-transparent bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text">
-        <h1 class="mt-1 text-4xl font-extrabold uppercase tracking-tighest sm:text-5xl lg:text-7xl" v-text="title" />
+      <div class="flex flex-col">
+        <h1
+          class="mt-1 text-4xl font-extrabold text-transparent uppercase  tracking-tighest sm:text-5xl lg:text-7xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text"
+          v-text="title"
+        />
 
-        <h2 class="order-first font-medium tracking-wide" v-text="subtitle" />
+        <h2
+          class="order-first font-medium tracking-wide text-transparent  bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text"
+          v-text="subtitle"
+        />
       </div>
 
       <p class="max-w-2xl mx-auto mt-8 text-xl font-medium text-white">
