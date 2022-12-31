7ad44d9e0b1677c019baeab5d053ad9bca867007
Mark Mead <itsmarkmead@gmail.com>
2022-12-31 17:47:24 +0000

    Remove links to my Twitter

--- components/HeaderMark.vue
+++ components/HeaderMark.vue
@@ -1,5 +1,5 @@
 <template>
-  <a href="https://twitter.com/itsmarkmead" target="_blank" rel="noreferrer">
+  <a href="https://github.com/markmead" target="_blank" rel="noreferrer">
     Mark
   </a>
 </template>
