53fda9cb6ed72935590bd94af2b365e592ba1ed7
jordihales <jordihales@hotmail.co.uk>
2024-05-11 23:55:29 +0100

    misc: change ad mode to dark

--- components/EthicalAds.vue
+++ components/EthicalAds.vue
@@ -2,5 +2,5 @@
     <div 
         data-ea-publisher="hypercolordev" 
         data-ea-type="image"
-        class="adaptive raised horizontal"></div>
+        class="dark raised horizontal"></div>
 </template>
